5384483c313c676b271ccd745dbdaef2c36f1e6e
Jane <jane.m.maguire@gmail.com>
2021-09-12 20:48:39 +0100

    Adds text, map and styles

--- airquality/README.md
+++ airquality/README.md
@@ -1,49 +1,15 @@
-# Instructions
+# Air Quality in Indian cities
 
-You have been given some data by the editorial department in the `./src/data` directory.
+This project displays an interactive map so users can select their city and see it's air quality.
+The project uses the Google Maps JavaScript API and es5 JavaScript.
 
-They need you to build an interactive project where users will select an Indian city and have their local air quality displayed in cigarettes and particulate matter.
+## Running
 
-How the users select their city is up to you: editorial have used dropdowns, buttons, maps, and keyword entry autocomplete text fields before. It's your choice.
+The project is built using npm and parcel. Run `npm install` and `parcel src/index.html` to run locally.
+If a map does not appear, refresh the page.
 
-## Techincal requirements
+## Future improvements
 
-The task must be served as all flat files so that the solution can scale in a serverless environment - e.g. we would put the HTML, CSS, and JS into S3 and serve statically
-
-The task _could_ use node, npm, webpack, babel etc to compile/transpile into the end project files for deployment. Whatever environment you are comfortable with, but no jquery, please. If you prefer, you can build a webpage by hand - it's up to you. 
-
-The project is going to go live in areas that do not have good broadband access, so please pay attention to file size and performance.
-
-Some users may have older phones, so please ensure your code works on IE11 and up.
-
-## Project requirements
-
-Must have: 
-- Ability for users to choose a city and see air quality in the form of cigarettes and particulate matter (PM2.5)
-- all text fields from the data json file displaying in a logical manner
-- All production files output into a `./dist` folder ready for making live.
-
-Nice to have:
-- A version in hindi (data also supplied in separate JSON file)
-- Test cases for your code.
-
-## What we're looking for
-
-Ideally, we're looking for a logical approach to the work, some creativity, beautiful code and a nice output, but we are not expecting a finished product by a long chalk.
-
-Don't spend too long on it. Try and cap it at 4 hours.
-
-This really is an opportunity for us to see how you think, how you organise yourself, how you express yourself. Have fun!
-
-
-## Help
-
-If you have any issues with this assessment or require some clarification then
-please email your contact at the BBC.
-
-## Submission
-
-Please send your code together with a Readme file explaining how to install/run
-the project and any other notes as a Zip file or a GitHub repository. Please
-also confirm that we have received the test (it may get blocked by spam
-filters).
+- Fix an issue with my JavaScript sometimes loading before the Google Maps API. This means occasionally the map does not appear on the initial load.
+- Display all cities in the json file by creating a reusable function for displaying city markers on the map. lat/lng co-ordinates could be added to the json or created as an Array.
+- Display the number of cigarettes by displaying the cigarette icon the corresponding number of times.
--- airquality/src/index.js
+++ airquality/src/index.js
@@ -1,1 +1,134 @@
-console.log('hello work');
+import data from './data/english.json'
+
+// Creates text elements
+var hero_image = document.getElementById("hero_image");
+var hero_title = document.getElementById("hero_title");
+var byline = document.getElementById("byline");
+var date = document.getElementById("date");
+var category = document.getElementById("category");
+var category_url = document.getElementById("category_url");
+var p_1 = document.getElementById("p_1");
+var p_2 = document.getElementById("p_2");
+var p_3 = document.getElementById("p_3");
+var p_4 = document.getElementById("p_4");
+var p_5 = document.getElementById("p_5");
+var p_6 = document.getElementById("p_6");
+var p_7 = document.getElementById("p_7");
+var p_8 = document.getElementById("p_8");
+var p_9 = document.getElementById("p_9");
+var p_10 = document.getElementById("p_10");
+var compare_method = document.getElementById("compare_method");
+var compare_title = document.getElementById("compare_title");
+
+// Sets text
+hero_image.setAttribute("href", data["hero_1_image"]);
+hero_title.innerHTML = data["hero_1_title"];
+byline.innerHTML = data["article-info_1_byline"];
+date.innerHTML = data["article-info_1_date"];
+category.innerHTML = data["article-info_1_category"];
+category_url.setAttribute("href", data["article-info_1_category_url"]);
+p_1.innerHTML = data["p_1_value"];
+p_2.innerHTML = data["p_2_value"];
+p_3.innerHTML = data["p_3_value"];
+p_4.innerHTML = data["p_4_value"];
+p_5.innerHTML = data["p_5_value"];
+p_6.innerHTML = data["p_6_value"];
+p_7.innerHTML = data["p_7_value"];
+p_8.innerHTML = data["p_8_value"];
+p_9.innerHTML = data["p_9_value"];
+p_10.innerHTML = data["p_10_value"];
+compare_method.innerHTML = data["compare-tabs_1_method"];
+compare_title.innerHTML = data["compare-tabs_1_title"];
+
+// Creates map
+var map;
+
+map = new google.maps.Map(document.getElementById("map"), {
+  center: { lat: 28, lng: 80 },
+  zoom: 7,
+});
+
+// Sets map markers
+// City 1
+var city_1 = new google.maps.Marker({
+  position: { lat: 28.6692, lng: 77.4538 },
+  map,
+});
+
+city_1.addListener("click", () => {
+  document.getElementById("city_info").innerHTML = '';
+  city_info.innerHTML = data["compare-tabs_1_city_1_name"] + '<br>' +
+                       data["compare-tabs_1_city_1_aqi"] + '<br>' +
+                       data["compare-tabs_1_city_1_cigg"] + ' cigarettes a day';
+  document.getElementById("info").appendChild(city_info);
+});
+
+// City 2
+var city_2 = new google.maps.Marker({
+  position: { lat: 28.5355, lng: 77.3910 },
+  map,
+});
+
+city_2.addListener("click", () => {
+  document.getElementById("city_info").innerHTML = '';
+  city_info.innerHTML = data["compare-tabs_1_city_2_name"] + '<br>' +
+                       data["compare-tabs_1_city_2_aqi"] + '<br>' +
+                       data["compare-tabs_1_city_2_cigg"] + ' cigarettes a day';
+  document.getElementById("info").appendChild(city_info);
+});
+
+// City 3
+var city_3 = new google.maps.Marker({
+  position: { lat: 28.7041, lng: 77.1025 },
+  map,
+});
+
+city_3.addListener("click", () => {
+  document.getElementById("city_info").innerHTML = ''
+  city_info.innerHTML = data["compare-tabs_1_city_3_name"] + '<br>' +
+                       data["compare-tabs_1_city_3_aqi"] + '<br>' +
+                       data["compare-tabs_1_city_3_cigg"] + ' cigarettes a day';
+  document.getElementById("info").appendChild(city_info);
+});
+
+// City 4
+var city_4 = new google.maps.Marker({
+  position: { lat: 26.8467, lng: 80.9462 },
+  map,
+});
+
+city_4.addListener("click", () => {
+  document.getElementById("city_info").innerHTML = ''
+  city_info.innerHTML = data["compare-tabs_1_city_4_name"] + '<br>' +
+                       data["compare-tabs_1_city_4_aqi"] + '<br>' +
+                       data["compare-tabs_1_city_4_cigg"] + ' cigarettes a day';
+  document.getElementById("info").appendChild(city_info);
+});
+
+// City 5
+var city_5 = new google.maps.Marker({
+  position: { lat: 28.4595, lng: 77.0266 },
+  map,
+});
+
+city_5.addListener("click", () => {
+  document.getElementById("city_info").innerHTML = ''
+  city_info.innerHTML = data["compare-tabs_1_city_5_name"] + '<br>' +
+                       data["compare-tabs_1_city_5_aqi"] + '<br>' +
+                       data["compare-tabs_1_city_5_cigg"] + ' cigarettes a day';
+  document.getElementById("info").appendChild(city_info);
+});
+
+// City 6
+var city_6 = new google.maps.Marker({
+  position: { lat: 28.4089, lng: 77.3178 },
+  map,
+});
+
+city_6.addListener("click", () => {
+  document.getElementById("city_info").innerHTML = ''
+  city_info.innerHTML = data["compare-tabs_1_city_6_name"] + '<br>' +
+                       data["compare-tabs_1_city_6_aqi"] + '<br>' +
+                       data["compare-tabs_1_city_6_cigg"] + ' cigarettes a day';
+  document.getElementById("info").appendChild(city_info);
+});
